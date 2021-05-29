2adb0c205f6f9617fe380609c9c69f50b81ec740
Arvin Alvea <arvinalvea@gmail.com>
2021-05-29 10:45:41 -0400

    Configured smtp.php

--- forms/smtp.php
+++ forms/smtp.php
@@ -20,22 +20,22 @@
 // (see cPanel -> email accounts).  Using an account at your host improves deliverability.
 
 // These are the Outgoing Server (SMTP) details provided by your email host
-$outgoingServerAddress     = 'server.company.com'; // consult your hosting provider.
-$outgoingServerPort        = '25';                  // '587' , '25' - consult your hosting provider
-$outgoingServerSecurity    = 'tls';                 // 'ssl' , 'tls' , null - consult your hosting provider.
+$outgoingServerAddress     = 'powerpackpaving.ca'; // consult your hosting provider.
+$outgoingServerPort        = '465';                  // '587' , '25' - consult your hosting provider
+$outgoingServerSecurity    = 'ssl';                 // 'ssl' , 'tls' , null - consult your hosting provider.
 
 // Sending Account Settings - replace these details with an email account held on the SMTP server entered above.
 // This will also be used as the account to send the confirmation to the user.
-$sendingAccountUsername    = 'insert_your_account_here';
-$sendingAccountPassword    = 'insert_your_password_here';
+$sendingAccountUsername    = 'info@powerpackpaving.ca';
+$sendingAccountPassword    = '9054707544';
 
 // Recipient (To:) Details - Change this to the email details of who will receive all the emails from the website.
-$recipientEmail            = 'recipient@email.com';      // Where to send the admin email.
-$recipientName             = 'Recipient Name'; // Name of admin to receive email from website.
+$recipientEmail            = 'arvinalvea@gmail.com';      // Where to send the admin email.
+$recipientName             = 'Anthony'; // Name of admin to receive email from website.
 
 // Email details - Change these to suit your website needs
 $emailSubject              = 'A message from a form on your website'; // Subject of the email that the admin will see.
-$websiteName               = 'Edit your company website name';       // This is used as the "From name".
+$websiteName               = 'Power Pack Paving';       // This is used as the "From name".
 $adminEmailTemplate        = 'email_to_admin.html';      // Name of template (in templates folder) to use for email to admin.
 
 // Success Message to display in browser
